d765fd4eb9704df4d30cd27c42485df09a404072
vicfriedman <victoria.m.friedman@gmail.com>
2015-08-27 16:17:57 -0400

    mass assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
+require 'pry'
 class Person
   #your code here
-end
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+  def initialize(hash)
+ 
+    # binding.pry
+   #  hash.each do |attribute, property|
+   #    self.public_send(attribute, property)
+   # end
+
+    hash.each do |attribute, property|
+      method_name = attribute.to_s + "="
+      public_send(method_name, property)
+    end
+
+ #end method
+  end
+end #end class
